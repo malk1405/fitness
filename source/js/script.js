91ad9830a11132767047c0758050f536f3f09ba5
malk <malk1405@mail.ru>
2020-03-28 21:19:40 +0300

    Сделал переключение слайдов

--- source/js/script.js
+++ source/js/script.js
@@ -87,8 +87,69 @@ function activateTabs() {
   });
 }
 
+function activateSlider() {
+  var WINDOW_SIZE = {
+    lg: { maxWidth: Infinity, num: 4 },
+    md: { maxWidth: 1365, num: 2 },
+    sm: { maxWidth: 767, num: 1 }
+  };
+
+  var index = 0;
+  function findMin(index, size) {
+    return Math.floor(index / size) * size;
+  }
+
+  var cards = document.querySelectorAll('.trainers__card');
+
+  function display(prefix) {
+    var className = 'trainers__card--hidden-' + prefix;
+    var min = findMin(index, WINDOW_SIZE[prefix].num);
+    for (var i = 0; i < cards.length; i++) {
+      if (i < min) cards[i].classList.add(className);
+      else cards[i].classList.remove(className);
+    }
+  }
+
+  function displayAll() {
+    display('lg');
+    display('md');
+    display('sm');
+  }
+
+  displayAll();
+
+  var prevBtn = document.querySelector('.trainers__prev');
+  var nextBtn = document.querySelector('.trainers__next');
+
+  function getWindowSize() {
+    if (window.innerWidth < WINDOW_SIZE.sm.maxWidth) return WINDOW_SIZE.sm.num;
+    if (window.innerWidth < WINDOW_SIZE.md.maxWidth) return WINDOW_SIZE.md.num;
+    return WINDOW_SIZE.lg.num;
+  }
+
+  prevBtn.addEventListener('click', function() {
+    var newIndex = index - getWindowSize();
+
+    if (newIndex < 0) return;
+
+    index = newIndex;
+    displayAll();
+  });
+
+  nextBtn.addEventListener('click', function() {
+    var size = getWindowSize();
+    var newIndex = findMin(index + size, size);
+
+    if (newIndex >= cards.length) return;
+
+    index = newIndex;
+    displayAll();
+  });
+}
+
 function app() {
   activateTabs();
+  activateSlider();
 }
 
 function docReady(fn) {
