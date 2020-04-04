014708134fe505d71648f5f8416522f18b0d9996
malk <malk1405@mail.ru>
2020-04-04 10:57:06 +0300

    Добавил в сборку генерацию файла vendors.js

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var del = require('del');
 
 var webpack = require('webpack-stream');
 
-gulp.task('css', function() {
+gulp.task('css', function () {
   return gulp
     .src('source/sass/style.scss')
     .pipe(plumber())
@@ -33,13 +33,13 @@ gulp.task('css', function() {
     .pipe(server.stream());
 });
 
-gulp.task('server', function() {
+gulp.task('server', function () {
   server.init({
     server: 'build/',
     notify: false,
     open: true,
     cors: true,
-    ui: false
+    ui: false,
   });
 
   gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
@@ -50,13 +50,19 @@ gulp.task('server', function() {
 });
 
 gulp.task('js', () => {
+  const entry = ['app', 'vendors'].reduce((acc, el) => {
+    acc[el] = `./source/js/${el}.js`;
+    return acc;
+  }, {});
+
   return gulp
-    .src('./source/js/app.js')
+    .src('./source/js/')
     .pipe(
       webpack({
+        entry,
         mode: 'development',
         output: {
-          filename: 'script.js'
+          filename: '[name].js',
         },
         watch: false,
         devtool: 'source-map',
@@ -74,47 +80,48 @@ gulp.task('js', () => {
                       {
                         debug: true,
                         corejs: 3,
-                        useBuiltIns: 'usage'
-                      }
-                    ]
-                  ]
-                }
-              }
-            }
-          ]
-        }
+                        useBuiltIns: 'usage',
+                      },
+                    ],
+                  ],
+                  plugins: ['@babel/plugin-transform-runtime'],
+                },
+              },
+            },
+          ],
+        },
       })
     )
     .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('refresh', function(done) {
+gulp.task('refresh', function (done) {
   server.reload();
   done();
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp
     .src('source/img/**/*.{png,jpg,svg}')
     .pipe(
       imagemin([
         imagemin.optipng({ optimizationLevel: 3 }),
         imagemin.jpegtran({ progressive: true }),
-        imagemin.svgo()
+        imagemin.svgo(),
       ])
     )
 
     .pipe(gulp.dest('source/img'));
 });
 
-gulp.task('webp', function() {
+gulp.task('webp', function () {
   return gulp
     .src('source/img/**/*.{png,jpg}')
     .pipe(webp({ quality: 90 }))
     .pipe(gulp.dest('source/img'));
 });
 
-gulp.task('sprite', function() {
+gulp.task('sprite', function () {
   return gulp
     .src('source/img/{icon-*,htmlacademy*}.svg')
     .pipe(svgstore({ inlineSvg: true }))
@@ -122,22 +129,22 @@ gulp.task('sprite', function() {
     .pipe(gulp.dest('build/img'));
 });
 
-gulp.task('html', function() {
+gulp.task('html', function () {
   return gulp
     .src('source/*.html')
     .pipe(posthtml([include()]))
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('copy', function() {
+gulp.task('copy', function () {
   return gulp
     .src(['source/fonts/**/*.{woff,woff2}', 'source/img/**', 'source//*.ico'], {
-      base: 'source'
+      base: 'source',
     })
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return del('build');
 });
 
