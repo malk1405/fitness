464a24712afb0167e3f6d96ae373adc4a2beed57
malk <malk1405@mail.ru>
2020-04-06 18:12:56 +0300

    Убрал класс с формы и сделал валидацию поля с телефоном

--- source/js/components/form.js
+++ source/js/components/form.js
@@ -1,11 +1,22 @@
 const activateForm = () => {
-  const form = document.querySelector('.contacts-form');
+  const form = document.querySelector('.contacts-form form');
   if (!form) return;
 
-  form.addEventListener('submit', event => {
+  let phoneText = '';
+
+  form.addEventListener('submit', (event) => {
     event.preventDefault();
     window.alert('Данные отправлены');
     form.reset();
+    phoneText = '';
+  });
+
+  const phoneField = form.querySelector('#contacts-form__phone');
+
+  phoneField.addEventListener('input', (event) => {
+    const value = event.target.value;
+    phoneText = /^[0-9]*$/.test(value) ? value : phoneText;
+    phoneField.value = phoneText;
   });
 };
 
