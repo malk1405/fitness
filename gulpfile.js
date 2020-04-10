4b4eb6dd3491ec9de1f469cc9603c2549cd7a87f
malk <malk1405@mail.ru>
2020-04-10 18:24:09 +0300

    Переделал спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -105,14 +105,8 @@ gulp.task('refresh', function (done) {
 
 gulp.task('images', function () {
   return gulp
-    .src('source/img/**/*.{png,jpg,svg}')
-    .pipe(
-      imagemin([
-        imagemin.optipng({ optimizationLevel: 3 }),
-        imagemin.jpegtran({ progressive: true }),
-        imagemin.svgo(),
-      ])
-    )
+    .src('source/img/**/*.{svg}')
+    .pipe(imagemin([imagemin.svgo()]))
 
     .pipe(gulp.dest('source/img'));
 });
@@ -126,9 +120,9 @@ gulp.task('webp', function () {
 
 gulp.task('sprite', function () {
   return gulp
-    .src('source/img/{icon-*,htmlacademy*}.svg')
+    .src('source/img/*.svg')
     .pipe(svgstore({ inlineSvg: true }))
-    .pipe(rename('sprite_auto.svg'))
+    .pipe(rename('sprite.svg'))
     .pipe(gulp.dest('build/img'));
 });
 
@@ -141,9 +135,16 @@ gulp.task('html', function () {
 
 gulp.task('copy', function () {
   return gulp
-    .src(['source/fonts/**/*.{woff,woff2}', 'source/img/**', 'source//*.ico'], {
-      base: 'source',
-    })
+    .src(
+      [
+        'source/fonts/**/*.{woff,woff2}',
+        'source/img/**/*.{webp,jpg,png}',
+        'source//*.ico',
+      ],
+      {
+        base: 'source',
+      }
+    )
     .pipe(gulp.dest('build'));
 });
 
